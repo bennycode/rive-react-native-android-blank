5fee5f19ca8206432a77af03fae6808882cf47f0
Mortadha Fadhlaoui <mortadhafadhlaoui@gmail.com>
2023-06-15 19:24:37 +0200

    add  custom asset plugin

--- App.js
+++ App.js
@@ -1,15 +1,9 @@
 import React, { useRef } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Button } from "react-native";
-import { useAssets } from "expo-asset";
 import Rive from "rive-react-native";
 
 export default function App() {
-  const [assets, error] = useAssets([
-    require("./assets/downloadAnimation.riv"),
-  ]);
-  const url = assets?.[0]?.localUri;
-
   const riveRef = useRef(null);
 
   const handleStop = () => {
@@ -22,28 +16,24 @@ export default function App() {
     riveRef.current?.fireState("downloadAnimation", "downloaded");
   };
 
-  console.log(url);
-
   return (
     <View style={styles.container}>
-      {url && (
-        <Rive
-          style={{ backgroundColor: "red", flex: 1, margin: 20 }}
-          ref={riveRef}
-          url={url}
-          autoplay={false}
-          stateMachineName="downloadAnimation"
-          onStateChanged={(stateMachineName, stateName) => {
-            console.log(
-              "onStateChanged: ",
-              "stateMachineName: ",
-              stateMachineName,
-              "stateName: ",
-              stateName
-            );
-          }}
-        />
-      )}
+      <Rive
+        style={{ backgroundColor: "red", flex: 1, margin: 20 }}
+        ref={riveRef}
+        resourceName="download_animation"
+        autoplay={false}
+        stateMachineName="downloadAnimation"
+        onStateChanged={(stateMachineName, stateName) => {
+          console.log(
+            "onStateChanged: ",
+            "stateMachineName: ",
+            stateMachineName,
+            "stateName: ",
+            stateName
+          );
+        }}
+      />
       <Button onPress={handleStop} title="Stop" />
       <Button onPress={handlePlay} title="Play" />
       <Button onPress={handleDownloaded} title="Downloaded" />
