e798322d9f5d322b0bc8c027f18e92b17efbfed2
Mortadha Fadhlaoui <mortadhafadhlaoui@gmail.com>
2023-06-17 09:58:41 +0200

    update to make ios work

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React, { useRef } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Button } from "react-native";
+import { StyleSheet, View, Button, Platform } from "react-native";
+import { useAssets } from "expo-asset";
 import Rive from "rive-react-native";
 
 export default function App() {
   const riveRef = useRef(null);
-
+  const [assets, error] = useAssets([
+    require("./assets/riv/download_animation.riv"),
+  ]);
+  const isIos = Platform.OS === "ios";
+  const url = isIos ? assets?.[0]?.localUri : undefined;
+  console.log({ url });
   const handleStop = () => {
     riveRef.current?.stop();
   };
@@ -18,22 +24,41 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Rive
-        style={{ backgroundColor: "red", flex: 1, margin: 20 }}
-        ref={riveRef}
-        resourceName="download_animation"
-        autoplay={false}
-        stateMachineName="downloadAnimation"
-        onStateChanged={(stateMachineName, stateName) => {
-          console.log(
-            "onStateChanged: ",
-            "stateMachineName: ",
-            stateMachineName,
-            "stateName: ",
-            stateName
-          );
-        }}
-      />
+      {url !== "" && url ? (
+        <Rive
+          style={{ backgroundColor: "red", flex: 1, margin: 20 }}
+          ref={riveRef}
+          url={url}
+          autoplay={false}
+          stateMachineName="downloadAnimation"
+          onStateChanged={(stateMachineName, stateName) => {
+            console.log(
+              "onStateChanged: ",
+              "stateMachineName: ",
+              stateMachineName,
+              "stateName: ",
+              stateName
+            );
+          }}
+        />
+      ) : (
+        <Rive
+          style={{ backgroundColor: "red", flex: 1, margin: 20 }}
+          ref={riveRef}
+          resourceName="download_animation"
+          autoplay={false}
+          stateMachineName="downloadAnimation"
+          onStateChanged={(stateMachineName, stateName) => {
+            console.log(
+              "onStateChanged: ",
+              "stateMachineName: ",
+              stateMachineName,
+              "stateName: ",
+              stateName
+            );
+          }}
+        />
+      )}
       <Button onPress={handleStop} title="Stop" />
       <Button onPress={handlePlay} title="Play" />
       <Button onPress={handleDownloaded} title="Downloaded" />
