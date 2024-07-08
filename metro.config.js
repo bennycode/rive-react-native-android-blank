f9c3ba7743c4d4e51bad254fb6c264daad217b5d
Benny Neugebauer <mail@bennycode.com>
2024-07-08 16:31:33 +0200

    Directly adding new asset extension

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,6 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.assetExts = [...config.resolver.assetExts, "riv"];
+config.resolver.assetExts.push("riv");
 
 module.exports = config;
